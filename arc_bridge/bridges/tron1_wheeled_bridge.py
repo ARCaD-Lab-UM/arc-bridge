6a2455c8326811293e6e88221a14995fe8efe1ea
Zelin Shen <zelin@umich.edu>
2025-09-19 17:31:20 -0400

    visualization

--- arc_bridge/bridges/tron1_wheeled_bridge.py
+++ arc_bridge/bridges/tron1_wheeled_bridge.py
@@ -18,3 +18,33 @@ class Tron1WheeledBridge(Lcm2MujocoBridge):
         if len(self.mj_data.qpos) > 11:
             self.low_state.q_ob = self.mj_data.qpos[11:11+3]
             self.low_state.dq_ob = self.mj_data.qvel[11:11+3]
+
+    def lcm_state_handler(self, channel, data):
+        if self.mj_data == None:
+            return
+        # Get state msg from robot SDK topic
+        msg = eval(self.topic_state+"_t").decode(data)
+
+        # Update mj_data for visualization
+        self.mj_data.qpos[0] = msg.position[0] # self.low_state.position[0]
+        self.mj_data.qpos[1] = msg.position[1] # self.low_state.position[1]
+        self.mj_data.qpos[2] = 1.0 # msg.position[2] # self.low_state.position[2]
+        self.mj_data.qpos[3] = msg.quaternion[0]
+        self.mj_data.qpos[4] = msg.quaternion[1]
+        self.mj_data.qpos[5] = msg.quaternion[2]
+        self.mj_data.qpos[6] = msg.quaternion[3]
+        self.mj_data.qpos[7:7+8] = msg.qj_pos - self.joint_offsets
+        self.mj_data.qvel[:] = 0
+
+        # Partially update low_state, for state estimator to use
+        # self.low_state.qj_pos[:] = (np.array(msg.qj_pos) + self.joint_offsets).tolist() # ! This one needs offsets since it should match with controller's model
+        self.low_state.qj_pos[:] = msg.qj_pos
+        self.low_state.qj_vel[:] = msg.qj_vel
+        self.low_state.qj_tau[:] = msg.qj_tau
+        self.low_state.acceleration[:] = msg.acceleration
+        self.low_state.omega[:] = msg.omega
+        self.low_state.quaternion[:] = msg.quaternion
+        self.low_state.rpy[:] = msg.rpy
+
+        self.low_state.position[:] = msg.position
+        self.low_state.velocity[:] = msg.velocity
