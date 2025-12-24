c72efe331ce314be148ffae4a16016a62e46fce0
Zelin Shen <zelin@umich.edu>
2025-12-24 18:29:14 -0500

    Revert floating base lin KF for other (non tron1 wf) robot

--- arc_bridge/state_estimators/floating_base_linear_state_estimator.py
+++ arc_bridge/state_estimators/floating_base_linear_state_estimator.py
@@ -7,7 +7,7 @@ class FloatingBaseLinearStateEstimator(KalmanFilter):
     def __init__(self, dt, Q, R, height_init):
         dim_state = 6   # (px, py, pz, vx, vy, vz)
         dim_control = 3 # (ax, ay, az)
-        dim_obs = 6     # (px, py, pz, vx, vy, vz)
+        dim_obs = 4     # (pz, vx, vy, vz)
         Ac = np.zeros((dim_state, dim_state))
         Ac[:3, 3:] = np.eye(3)
         Bc = np.zeros((dim_state, dim_control))
@@ -17,7 +17,8 @@ class FloatingBaseLinearStateEstimator(KalmanFilter):
 
         A = expm(dt * Ac)
         B = dt * Bc
-        C = np.eye(6)
+        C = np.zeros((dim_obs, dim_state))
+        C[:, 2:] = np.eye(4)
         # print(f"C:\n{C}")
 
         self.x_init = np.array([0, 0, height_init, 0, 0, 0])   # initial pos and vel in 3D
